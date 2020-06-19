2ec265167d8bd6c45f654b97cde95fdbcdcc5b4d
channadsc <60209466+channadsc@users.noreply.github.com>
2020-06-19 19:28:00 +0530

    write to the quote file

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,18 @@
 import random
 def primary():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
   lst = 13
   rnd = random.randint(0,lst)
+  rnd1 = random.randint(0,lst)
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   print(quotes)
-  print(quotes[rnd])
+  print(quotes[rnd]+quotes[rnd1])
+  with open("quotes.txt",'a') as q:
+  	q.write("\nFlyhigh in the sky\n")
+  print("")
 
 if __name__== "__main__":
   primary()
